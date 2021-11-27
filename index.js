a64b39aea540c8e31e01a330191662d5013fb0d8
guijun13 <guilhermejun13.grillo@gmail.com>
2021-11-26 21:36:35 -0300

    init crawler

---
+++ index.js
@@ -0,0 +1,146 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch({ ignoreHTTPSErrors: true });
+  // const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  const navigationPromise = page.waitForNavigation();
+
+  // const url = `http://www.biblioteca.presidencia.gov.br/presidencia/ex-presidentes`;
+  const url = `http://www.biblioteca.presidencia.gov.br/mapadosite`;
+  await page.goto(url);
+
+  await navigationPromise;
+
+  // await page.waitForSelector(
+  //   '#portal-column-content #content .row .cell .tile-default .tile-content a'
+  // );
+  // await page.click('#portal-column-content #content .row .cell .tile-default .tile-content a');
+
+  await page.waitForSelector(
+    '#portal-sitemap > li:nth-child(10) > ul > li:nth-child(1) > ul > li > div'
+  );
+  // await page.click('#portal-sitemap > li:nth-child(10) > ul > li:nth-child(1) > ul > li > div > a');
+
+  // await page.waitForSelector(
+  //   'div > ul > li > span > span > span > span > span > span > span > span > span > span > span > span > a'
+  // );
+  // await page.click(
+  //   'div > ul > li > span > span > span > span > span > span > span > span > span > span > span > span > a'
+  // );
+
+  // await page.waitForSelector('#content-core > article:nth-child(1) > div > h2 > a');
+  // await page.click('#content-core > article:nth-child(1) > div > h2 > a');
+
+  const linksList = await page.evaluate(() => {
+    const results = [];
+    const items = document.querySelectorAll(
+      '#portal-sitemap > li:nth-child(10) > ul > li:nth-child(1) > ul > li > div'
+    );
+    items.forEach((item) => {
+      results.push({
+        url: item.children[0].href,
+      });
+    });
+    return results;
+  });
+
+  // console.log(linksList);
+
+  // for (let i = 0; i < linksList.length; i++) {
+  for (let i = 0; i < 1; i++) {
+    const link = linksList[i];
+    await page.goto(`${link.url}`);
+    await navigationPromise;
+
+    const discoursesResults = await page.evaluate(() => {
+      const yearsLinks = [];
+      if (document.querySelectorAll('#f0046f8a875947359dac10a807564bc3')) {
+        // tem a parte de 'Discursos presidenciais'
+        if (document.querySelector('#f84b41b10df14a67a7250c2b2bf06c07 > div > ul > li')) {
+          // tem uma lista de anos
+          const itens = document.querySelectorAll(
+            '#f84b41b10df14a67a7250c2b2bf06c07 > div > ul > li a'
+          );
+          itens.forEach((i) => {
+            yearsLinks.push({
+              url: i.href,
+            });
+          });
+          return yearsLinks;
+        } else if (document.querySelector('#f84b41b10df14a67a7250c2b2bf06c07 > div a')) {
+          // tem so 1 link
+          return document.querySelector('#f84b41b10df14a67a7250c2b2bf06c07 > div a:first-child')
+            .href;
+        }
+      } else {
+        return;
+      }
+    });
+
+    let lenDiscoursesResults;
+    if (discoursesResults) {
+      if (discoursesResults.length > 1) {
+        lenDiscoursesResults = discoursesResults.length;
+      } else {
+        lenDiscoursesResults = 1;
+      }
+    }
+
+    for (let j = 0; j < lenDiscoursesResults; j++) {
+      let link = discoursesResults[j];
+      if (link.url != null && link.url != undefined) {
+        // console.log(`${link.url}`);
+        await page.goto(`${link.url}`, { waitUntil: 'networkidle2' }).catch((e) => void 0);
+        await navigationPromise;
+
+        await page.evaluate(() => {
+          // pegar o numero de paginas
+          if (document.querySelector('#content-core > ul > li:nth-last-child(2)') == null) {
+            return;
+          }
+          pagesToScrape = document.querySelector(
+            '#content-core > ul > li:nth-last-child(2)'
+          ).innerText;
+        });
+
+        let currentPage = 1;
+        let pagesToScrape;
+        let urls = [];
+
+        if (!pagesToScrape) {
+          pagesToScrape = 1;
+        }
+
+        while (currentPage <= pagesToScrape) {
+          let discourseList = await page.evaluate(() => {
+            let discourseArticleList = [];
+            let items = document.querySelectorAll('#content-core a.summary.url');
+            items.forEach((item) => {
+              discourseArticleList.push({
+                url: item.href,
+              });
+            });
+            return discourseArticleList;
+          });
+          urls = urls.concat(discourseList);
+          if (currentPage < pagesToScrape) {
+            await Promise.all([
+              await page.click('#content-core > ul > li:last-child'),
+              await page.waitForSelector('#content-core a.summary.url'),
+            ]);
+          }
+          currentPage++;
+          console.log(currentPage);
+          console.log(pagesToScrape);
+
+          console.log(urls);
+        }
+      }
+    }
+  }
+
+  // await navigationPromise;
+
+  await browser.close();
+})();
