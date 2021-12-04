421168c1a513d615349c441ba5a27f5a371c310a
guijun13 <guilhermejun13.grillo@gmail.com>
2021-12-04 17:42:10 -0300

    feat: update functions

--- rename-files.js
+++ rename-files.js
@@ -1,23 +1,37 @@
 const fs = require('fs');
 const path = require('path');
 
-let getDirName = require('path').dirname;
-
 // changing the name of files to nameSurname_index.txt
-const oldPath = __dirname + '/pdf-txt-copia/fernandocollor/';
+const oldPath = __dirname + '/pdf-txt-copia/luizinácioluladasilva/';
 
+// remove all files from 'year' folders, renaming them
 (async () => {
   try {
-    const data = await fs.promises.readdir(oldPath);
-    // for (const year of yearsFolder) {
-    // const data = await fs.promises.readdir(`${oldPath}${year}/`);
+    const yearsFolder = await fs.promises.readdir(oldPath);
+    for (const year of yearsFolder) {
+      const data = await fs.promises.readdir(`${oldPath}${year}/`);
+      for (const [index, file] of data.entries()) {
+        const fromPath = path.join(`${oldPath}/${year}/`, file);
+        fs.rename(fromPath, `${oldPath}${Math.random(index)}_gou.txt`, (err) => {
+          console.log(err == null ? 'deu bom 1' : `segunda func: ${err}`);
+        });
+      }
+    }
+  } catch (e) {
+    console.error(e);
+  }
+})();
+
+//  rename all files to president name pattern
+(async () => {
+  try {
+    const data = await fs.promises.readdir(`${oldPath}`);
     for (const [index, file] of data.entries()) {
       const fromPath = path.join(`${oldPath}`, file);
-      fs.rename(fromPath, `${oldPath}fernandoCollor_${index}.txt`, () => {
-        console.log('File Renamed!');
+      fs.rename(fromPath, `${oldPath}luizInacioLula_${index}.txt`, (err) => {
+        console.log(err == null ? 'deu bom 2' : `segunda func: ${err}`);
       });
     }
-    // }
   } catch (e) {
     console.error(e);
   }
